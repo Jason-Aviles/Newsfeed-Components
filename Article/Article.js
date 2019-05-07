c72ebaa35e313d1b00ac64ccc7386f3387dbce38
Jason Aviles <sandman2k18@outlook.com>
2019-05-07 19:32:26 -0400

    toggle menu done now working on article.js

--- Article/Article.js
+++ Article/Article.js
@@ -3,11 +3,11 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandArticle
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
@@ -26,4 +26,12 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+console.log(articles)
+
+articles.forEach(
+  (domElement)=>{
+    return new Article(domElement)
+  }
+)
